54e0bc3405b989e2a4ce268a4ad2b3378d64a00c
tang <119taeyoung@naver.com>
2023-07-06 22:11:25 +0900

    theme with styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
+
 import styled from 'styled-components';
+
+const Container = styled.div`
+  background-color: ${props => props.theme.bgColor}
+`;
+
+const H1 = styled.h1`
+  color: ${props => props.theme.textColor}
+`;
+
 
 function App() {
   return (
-    <div>APP</div>
+   <Container>
+    <H1>
+      protected
+    </H1>
+   </Container>
   );
 }
 
---
+++ src/theme.ts
@@ -0,0 +1,13 @@
+import { DefaultTheme } from "styled-components";
+
+export const lightTheme:DefaultTheme = {
+    textColor: "black",
+    bgColor: "white",
+    btnColor: "pink"
+}
+
+export const darkTheme:DefaultTheme = {
+    textColor: "white",
+    bgColor: "black",
+    btnColor: "beige"
+}
