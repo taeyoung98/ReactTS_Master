3087cf7db816b2e29bad21ea8edb90f7b459fbe6
tang <119taeyoung@naver.com>
2023-07-13 05:37:56 +0900

    TodoList: selector

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,26 +1,30 @@
-import { useRecoilValue } from "recoil"
-import { toDoState } from "../atoms"
+import { useRecoilState, useRecoilValue } from "recoil"
+import { categoryState, toDoSelector } from "../atoms"
 import CreateToDo from "./CreateTodo"
 import ToDo from "./ToDo"
 
 function ToDoList() {
-  const toDos = useRecoilValue(toDoState)
-  console.log(toDos)
+  const toDos = useRecoilValue(toDoSelector)
+  const [category, setCategory] = useRecoilState(categoryState)
+  const onInput = (event: React.FormEvent<HTMLSelectElement>) => {
+    setCategory(event.currentTarget.value)
+  }
 
   return (
     <div>
       <h1>To Dos</h1>
       <hr />
+
+      <select value={category} onInput={onInput}>
+        <option value="TO_DO">To Do</option>
+        <option value="DOING">Doing</option>
+        <option value="DONE">Done</option>
+      </select>
       <CreateToDo />
-      <ul>
-        {toDos.map((toDo) => (
-          // NOTE
-          // toDos[](toDoState)의 아이템과 <ToDo>의 props가 둘 다 IToDo로 key 완전 일치하기 때문에
-          // {...toDo} 로 props 전달 가능
-          <ToDo key={toDo.id} {...toDo} />
-          // <ToDo key={toDo.id} id={toDo.id} text={toDo.text} category={toDo.category} />
-        ))}
-      </ul>
+
+      {toDos?.map((toDo) => (
+        <ToDo key={toDo.id} {...toDo} />
+      ))}
     </div>
   )
 }
