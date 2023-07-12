955dd539dacb4eabb7b78c2513949467cd1ccf10
tang <119taeyoung@naver.com>
2023-07-13 05:47:00 +0900

    TodoList: enum

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,11 +1,11 @@
 import { useSetRecoilState } from "recoil"
-import { IToDo, toDoState } from "../atoms"
+import { Categories, IToDo, toDoState } from "../atoms"
 
 function ToDo({ id, text, category }: IToDo) {
   const setToDos = useSetRecoilState(toDoState)
 
   // HOW-TO const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-  const onClick = (inputCategory: IToDo["category"]) => {
+  const onClick = (clickCategory: IToDo["category"]) => {
     // HOW-TO const { currentTarget: { name }} = event
 
     // 1. 수정하려는 아이템 (인덱스) 찾기
@@ -13,7 +13,7 @@ function ToDo({ id, text, category }: IToDo) {
       const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id)
       const oldToDos_front = oldToDos.slice(0, targetIndex)
       const oldToDos_back = oldToDos.slice(targetIndex + 1)
-      const newToDo = { id, text, category: inputCategory }
+      const newToDo = { id, text, category: clickCategory }
 
       return [...oldToDos_front, newToDo, ...oldToDos_back]
     })
@@ -21,9 +21,9 @@ function ToDo({ id, text, category }: IToDo) {
   return (
     <li>
       <span>{text}</span>
-      {category !== "TO_DO" && <button onClick={() => onClick("TO_DO")}>To Do</button>}
-      {category !== "DOING" && <button onClick={() => onClick("DOING")}>Doing</button>}
-      {category !== "DONE" && <button onClick={() => onClick("DONE")}>Done</button>}
+      {category !== Categories.TO_DO && <button onClick={() => onClick(Categories.TO_DO)}>To Do</button>}
+      {category !== Categories.DOING && <button onClick={() => onClick(Categories.DOING)}>Doing</button>}
+      {category !== Categories.DONE && <button onClick={() => onClick(Categories.DONE)}>Done</button>}
     </li>
   )
 }
--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,5 +1,5 @@
 import { useRecoilState, useRecoilValue } from "recoil"
-import { categoryState, toDoSelector } from "../atoms"
+import { Categories, categoryState, toDoSelector } from "../atoms"
 import CreateToDo from "./CreateTodo"
 import ToDo from "./ToDo"
 
@@ -7,7 +7,7 @@ function ToDoList() {
   const toDos = useRecoilValue(toDoSelector)
   const [category, setCategory] = useRecoilState(categoryState)
   const onInput = (event: React.FormEvent<HTMLSelectElement>) => {
-    setCategory(event.currentTarget.value)
+    setCategory(event.currentTarget.value as any)
   }
 
   return (
@@ -16,9 +16,9 @@ function ToDoList() {
       <hr />
 
       <select value={category} onInput={onInput}>
-        <option value="TO_DO">To Do</option>
-        <option value="DOING">Doing</option>
-        <option value="DONE">Done</option>
+        <option value={Categories.TO_DO}>To Do</option>
+        <option value={Categories.DOING}>Doing</option>
+        <option value={Categories.DONE}>Done</option>
       </select>
       <CreateToDo />
 
